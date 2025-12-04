94acbb7c5fa2b7c6e5951ba8008084d5629bc9ee
Маргарита Белова <Margaritazhohova@mail.ru>
2025-12-04 10:42:36 +0500

    add current year

---
+++ project-1-todo/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const currentYear = new Date().getFullYear(); //декларативный (четко говорим, что текущий год это...)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount(count + 1)}> {/* императивный (при клике сделай это...)*/}
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <p>{currentYear}</p>
+    </> // декларативный (просто: покажи год)
+  );
+}
+
+export default App;
